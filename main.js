72c3fd4a77b87be05fa53ef9374f41d204f2d738
BuildTools <maciej.bodnar2@gmail.com>
2024-03-24 14:20:04 +0100

    adding files, basic html, js, css

---
+++ main.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-el");
+const btnEl = document.getElementById("btn-el");
+
+btnEl.addEventListener("click", function () {
+  let input = inputEl.value;
+  console.log(input);
+});
